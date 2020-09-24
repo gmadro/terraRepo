ff00b981a0650e1c53eb3333f9d60dd3386b9766
gmadro <greg.madro@gmail.com>
2020-09-24 12:28:53 -0400

    rev4

---
+++ rev4/infrastructure/terraform/main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "packer" {
+  owners = ["self"]
+
+  filter {
+    name = "name"
+    values = [var.image_name]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+terraform {
+    backend "s3" {
+        bucket = "handled-by-pipline"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "handled-by-pipline"
+    }
+}
+
+resource "aws_security_group" "allow_http_ssh" {
+  name = "${var.image_name}-allow_http_ssh"
+  description = " Allow HTTP and SSH traffic to this instance from ANYWHERE"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    description = "Custom HTTP from WORLD"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Custom SSH from WORLD"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow HTTP/SHH"
+  }
+}
+
+resource "aws_instance" "remote"{
+    ami = data.aws_ami.packer.id
+    instance_type = "t2.micro"
+    key_name = var.ssh_key
+
+    tags = {
+        Name = var.image_name
+    }
+    vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+}
